2b16fe4a210488b1f991a77461c3153c4d6dd29e
Gabriel Boscoli <gboscoli@tecgraf.puc-rio.br>
2020-10-17 19:43:42 -0300

    get vizinhos 3 funcionando. range 380-405

--- simulated-annealing.py
+++ simulated-annealing.py
@@ -7,7 +7,7 @@ Created on Fri Oct 16 13:00:32 2020
 Módulo responsável pelo simulated annealing
 """
 
-from random import choice, uniform, randint
+from random import choice, choices, uniform, randint
 from math import exp
 import copy
 
@@ -42,121 +42,6 @@ def solucaoValida(cavaleiros):
     if qntd_cavaleiros < maximo_cavaleiros:
         return True
     return False
-
-def geraEstadoInicial(cavaleiros, cavaleiros_faltando):    
-    # coloca todos os cavaleiros em uma casa, matando todos eles
-    index_casa = 0
-    for i in range(CAVALEIROS):
-        for j in range(VIDA):
-            cavaleiro = NOME_CAVALEIROS[i]
-            cavaleiros[index_casa % CASAS_DO_ZODIACO].append(cavaleiro)
-            cavaleiros_faltando[cavaleiro] -= 1
-            index_casa += 1
-            
-    print(cavaleiros)
-            
-    # salva aleatoriamente um cavaleiro
-    cavaleiro_salvo = NOME_CAVALEIROS[randint(0, CAVALEIROS)]
-    casas_com_cavaleiro = []
-    for i in range(CASAS_DO_ZODIACO):
-        if cavaleiro_salvo in cavaleiros[i] and len(cavaleiros[i]) > 1:
-            casas_com_cavaleiro.append(i)
-    cavaleiros[choice(casas_com_cavaleiro)].remove(cavaleiro_salvo)
-
-def trocaCasas(cavaleiros):
-    casa1 = randint(0,len(cavaleiros))
-    casa2 = randint(0,len(cavaleiros))
-    cavaleiros[casa1], cavaleiros[casa2] = cavaleiros[casa2], cavaleiros[casa1]
-    
-def shiftaCavaleiros(cavaleiros):
-    fileira = randint(0, 3)
-    primeiro_cavaleiro = None
-    # salva o cavaleiro da fileira da primeira casa
-    if fileira < len(cavaleiros[0]):
-        primeiro_cavaleiro = cavaleiros[0][fileira]
-        cavaleiros[0].remove(primeiro_cavaleiro)   
-    # para todos os outros cavaleiros
-    for i in range(1, len(cavaleiros)):
-        if fileira < len(cavaleiros[i]):
-            cavaleiro_removido = cavaleiros[i][fileira]
-            cavaleiros[i].remove(cavaleiro_removido)
-            # procura casa anterior que nao tem o caveleiro que foi removido e o adiciona
-            for j in range(1, CASAS_DO_ZODIACO):
-                    if cavaleiro_removido not in cavaleiros[(i - j) % CASAS_DO_ZODIACO]:
-                        cavaleiros[(i - j) % CASAS_DO_ZODIACO].append(cavaleiro_removido)
-    # se tem primeiro cavaleiro
-    for i in range(CASAS_DO_ZODIACO):
-        if primeiro_cavaleiro not in cavaleiros[(i - j) % CASAS_DO_ZODIACO]:
-            cavaleiros[(i - j) % CASAS_DO_ZODIACO].append(primeiro_cavaleiro)
-
-def trocaCavaleiro(cavaleiros, cavaleiros_faltando):
-    cavaleiro1 = NOME_CAVALEIROS[randint(0,len(NOME_CAVALEIROS))]
-    casas_sem_cavaleiro1 = []
-    casas_com_cavaleiro1 = []
-    # acha casa que nao tem o cavaleiro
-    for i in range(len(cavaleiros)):
-        if cavaleiro1 not in cavaleiros[i]:
-            casas_sem_cavaleiro1.append(i)
-        else:
-            casas_com_cavaleiro1.append(i)
-            
-    cavaleiro2 = NOME_CAVALEIROS[randint(0,len(NOME_CAVALEIROS))]
-    casas_sem_cavaleiro2 = []
-    casas_com_cavaleiro2 = []
-    # acha casa que nao tem o cavaleiro
-    for i in range(len(cavaleiros)):
-        if cavaleiro2 not in cavaleiros[i]:
-            casas_sem_cavaleiro2.append(i)
-        else:
-            casas_com_cavaleiro2.append(i)
-            
-    casa_sem_cavaleiro1 = choice(casas_sem_cavaleiro1)
-    casa_sem_cavaleiro2 = choice(casas_sem_cavaleiro2)
-    casa_com_cavaleiro1 = choice(casas_com_cavaleiro1)
-    casa_com_cavaleiro2 = choice(casas_com_cavaleiro2)
-    
-    # remove cavaleiro 1 da sua casa original e insere na nova
-    cavaleiros[casa_com_cavaleiro1].remove(cavaleiro1)
-    cavaleiros[casa_sem_cavaleiro1].append(cavaleiro1)
-    # remove cavaleiro 2 da sua casa original e insere na nova
-    cavaleiros[casa_com_cavaleiro2].remove(cavaleiro2)
-    cavaleiros[casa_sem_cavaleiro2].append(cavaleiro2)
-    
-    # verificar se essa vizinhança preserva o maximo e o minimo de cavaleiros
-    
-def trocaCavaleiroVivo(cavaleiros, cavaleiros_faltando):
-    cavaleiro_vivo = None
-    # pega o cavaleiro que está vivo
-    for i in range(NOME_CAVALEIROS):
-        if cavaleiros_faltando[NOME_CAVALEIROS[i]] > 0:
-            cavaleiro_vivo = NOME_CAVALEIROS[i]
-    
-    # escolhe cavaleiro para viver
-    cavaleiro_viver = NOME_CAVALEIROS[randint(0,len(NOME_CAVALEIROS))]
-    while(cavaleiro_viver == cavaleiro_vivo):
-        cavaleiro_viver = NOME_CAVALEIROS[randint(0,len(NOME_CAVALEIROS))]
-    
-    casas_com_cavaleiro = []
-    for i in range(len(cavaleiros)):
-        if cavaleiro_viver in cavaleiros[i]:
-            casas_com_cavaleiro.append(i)
-    
-    casa_com_cavaleiro = choice(casas_com_cavaleiro)
-    
-    # remove o cavaleiro que vai viver
-    cavaleiros[casa_com_cavaleiro].remove(cavaleiro_viver)
-    cavaleiros_faltando[cavaleiro_viver] += 1
-    
-    casas_sem_cavaleiro = []
-    for i in range(len(cavaleiros)):
-        if cavaleiro_vivo not in cavaleiros[i] and len(cavaleiros[i]) < MAX_CAVALEIROS_CASA:
-            casas_sem_cavaleiro.append(i)
-            
-    casa_sem_cavaleiro = choice(casas_sem_cavaleiro)
-    
-    # adiciona o cavaleiro que vai morrer
-    cavaleiros[casa_sem_cavaleiro].append(cavaleiro_vivo)
-    cavaleiros_faltando[cavaleiro_vivo] -= 1
 
 class SimulatedAnnealing:
     def __init__(self, dificuldade, cavaleiros, cavaleiros_faltando, current_state=0):
@@ -246,6 +131,151 @@ class SimulatedAnnealing:
                         neighbors.append(SimulatedAnnealing(self.dificuldade, aux_cavaleiros, aux_cavaleiros_faltando, self.current_state))
         return neighbors
     
+    def get_neighbors3(self):
+        operacoes = [self.shiftaCavaleiro, self.trocaCasas, self.trocaCavaleiroVivo, self.trocaCavaleiro, self.shiftaCavaleiro]
+        vizinhancas = 10
+        current_vizinhanca = 0
+        melhor_custo = 100000000
+        melhor_vizinhanca = None
+        while current_vizinhanca < vizinhancas:
+            operacao = choice(operacoes)
+            vizinho = operacao()
+            if (solucaoValida(vizinho.cavaleiros)):
+                current_vizinhanca += 1
+                if vizinho.get_cost() < melhor_custo:
+                    melhor_vizinhanca = vizinho
+        return melhor_vizinhanca
+    
+    def geraEstadoInicial(self):    
+        cavaleiros = copy.deepcopy(self.cavaleiros)
+        cavaleiros_faltando = copy.deepcopy(self.cavaleiros_faltando)
+        # coloca todos os cavaleiros em uma casa, matando todos eles
+        index_casa = 0
+        for i in range(CAVALEIROS):
+            for j in range(VIDA):
+                cavaleiro = NOME_CAVALEIROS[i]
+                cavaleiros[index_casa % CASAS_DO_ZODIACO].append(cavaleiro)
+                cavaleiros_faltando[cavaleiro] -= 1
+                index_casa += 1
+                
+        # salva aleatoriamente um cavaleiro
+        cavaleiro_salvo = NOME_CAVALEIROS[randint(0, CAVALEIROS - 1)]
+        casas_com_cavaleiro = []
+        for i in range(CASAS_DO_ZODIACO):
+            if cavaleiro_salvo in cavaleiros[i] and len(cavaleiros[i]) > 1:
+                casas_com_cavaleiro.append(i)
+        cavaleiros[choice(casas_com_cavaleiro)].remove(cavaleiro_salvo)
+        cavaleiros_faltando[cavaleiro_salvo] += 1
+        self.cavaleiros = cavaleiros
+        self.cavaleiros_faltando = cavaleiros_faltando
+    
+    def trocaCasas(self):
+        cavaleiros = copy.deepcopy(self.cavaleiros)
+        casa1 = randint(0,len(cavaleiros) - 1)
+        casa2 = randint(0,len(cavaleiros) - 1)
+        cavaleiros[casa1], cavaleiros[casa2] = cavaleiros[casa2], cavaleiros[casa1]
+        return SimulatedAnnealing(self.dificuldade, cavaleiros, self.cavaleiros_faltando, self.current_state)
+    
+    def shiftaCavaleiro(self):
+        cavaleiros = copy.deepcopy(self.cavaleiros)
+        fileira = randint(0, MAX_CAVALEIROS_CASA - 1)
+        casa = randint(0, len(cavaleiros) - 1)
+        
+        cavaleiro = None
+        if fileira < len(cavaleiros[casa]) and len(cavaleiros[casa]) > MIN_CAVALEIROS_CASA:
+            cavaleiro = cavaleiros[casa][fileira]
+            cavaleiros[casa].remove(cavaleiro)
+        else:
+            cavaleiro = cavaleiros[casa].pop()
+        
+        if cavaleiro:
+            while True:
+                index = (casa + 1) % len(cavaleiros)
+                if len(cavaleiros[index]) < MAX_CAVALEIROS_CASA and cavaleiro not in cavaleiros[index]:
+                    cavaleiros[index].append(cavaleiro)
+                    break
+                casa += 1
+        return SimulatedAnnealing(self.dificuldade, cavaleiros, self.cavaleiros_faltando, self.current_state)
+    
+    def trocaCavaleiro(self):
+        cavaleiros = copy.deepcopy(self.cavaleiros)
+        
+        # escolhe dois cavaleiros diferentes aleatoriamente
+        cavaleiro1, cavaleiro2 = choices(NOME_CAVALEIROS, k=2)
+        while (cavaleiro1 == cavaleiro2):
+            cavaleiro1, cavaleiro2 = choices(NOME_CAVALEIROS, k=2)
+
+        casas_sem_cavaleiro1 = []
+        casas_com_cavaleiro1 = []
+        # acha casa que nao tem o cavaleiro
+        for i in range(len(cavaleiros)):
+            if cavaleiro1 not in cavaleiros[i]:
+                casas_sem_cavaleiro1.append(i)
+            else:
+                casas_com_cavaleiro1.append(i)
+                
+        #cavaleiro2 = NOME_CAVALEIROS[randint(0,len(NOME_CAVALEIROS) - 1)]
+        casas_sem_cavaleiro2 = []
+        casas_com_cavaleiro2 = []
+        # acha casa que nao tem o cavaleiro
+        for i in range(len(cavaleiros)):
+            if cavaleiro2 not in cavaleiros[i]:
+                casas_sem_cavaleiro2.append(i)
+            else:
+                casas_com_cavaleiro2.append(i)
+                
+        casa_sem_cavaleiro1 = choice(casas_sem_cavaleiro1)
+        casa_sem_cavaleiro2 = choice(casas_sem_cavaleiro2)
+        casa_com_cavaleiro1 = choice(casas_com_cavaleiro1)
+        casa_com_cavaleiro2 = choice(casas_com_cavaleiro2)
+        
+        # remove cavaleiro 1 da sua casa original e insere na nova
+        cavaleiros[casa_com_cavaleiro1].remove(cavaleiro1)
+        cavaleiros[casa_sem_cavaleiro1].append(cavaleiro1)
+        # remove cavaleiro 2 da sua casa original e insere na nova
+        cavaleiros[casa_com_cavaleiro2].remove(cavaleiro2)
+        cavaleiros[casa_sem_cavaleiro2].append(cavaleiro2)
+        
+        return SimulatedAnnealing(self.dificuldade, cavaleiros, self.cavaleiros_faltando, self.current_state)
+        
+    def trocaCavaleiroVivo(self):
+        cavaleiros = copy.deepcopy(self.cavaleiros)
+        cavaleiros_faltando = copy.deepcopy(self.cavaleiros_faltando)
+        cavaleiro_vivo = None
+        
+        # pega o cavaleiro que está vivo
+        for i in range(len(NOME_CAVALEIROS)):
+            if cavaleiros_faltando[NOME_CAVALEIROS[i]] > 0:
+                cavaleiro_vivo = NOME_CAVALEIROS[i]
+                
+        # escolhe cavaleiro para viver
+        cavaleiro_viver = NOME_CAVALEIROS[randint(0,len(NOME_CAVALEIROS) - 1)]
+        while(cavaleiro_viver == cavaleiro_vivo):
+            cavaleiro_viver = NOME_CAVALEIROS[randint(0,len(NOME_CAVALEIROS) - 1)]
+        
+        casas_com_cavaleiro = []
+        for i in range(len(cavaleiros)):
+            if cavaleiro_viver in cavaleiros[i]:
+                casas_com_cavaleiro.append(i)
+        
+        casa_com_cavaleiro = choice(casas_com_cavaleiro)
+        
+        # remove o cavaleiro que vai viver
+        cavaleiros[casa_com_cavaleiro].remove(cavaleiro_viver)
+        cavaleiros_faltando[cavaleiro_viver] += 1
+        
+        casas_sem_cavaleiro = []
+        for i in range(len(cavaleiros)):
+            if cavaleiro_vivo not in cavaleiros[i] and len(cavaleiros[i]) < MAX_CAVALEIROS_CASA:
+                casas_sem_cavaleiro.append(i)
+                
+        casa_sem_cavaleiro = choice(casas_sem_cavaleiro)
+        
+        # adiciona o cavaleiro que vai morrer
+        cavaleiros[casa_sem_cavaleiro].append(cavaleiro_vivo)
+        cavaleiros_faltando[cavaleiro_vivo] -= 1
+        return SimulatedAnnealing(self.dificuldade, cavaleiros, cavaleiros_faltando, self.current_state)
+    
     def simulated_annealing(self):
         """Peforms simulated annealing to find a solution"""
         initial_temp = 100
@@ -255,12 +285,14 @@ class SimulatedAnnealing:
         current_temp = initial_temp
     
         # Start by initializing the current state with the initial state
+        self.geraEstadoInicial()
         current_state = self
         solution = current_state
         melhor_solucao = solution
     
         while current_temp > final_temp:
-            neighbor = choice(solution.get_neighbors2())
+            #neighbor = choice(solution.get_neighbors2())
+            neighbor = self.get_neighbors3()
     
             # Check if neighbor is best so far
             cost_diff = solution.get_cost() - neighbor.get_cost()
@@ -282,27 +314,25 @@ class SimulatedAnnealing:
 def main():
     dificuldade = [50, 55, 60, 70, 75, 80, 85, 90, 95, 100, 110, 120]
     cavaleiros = [[], [], [], [], [], [], [], [], [], [], [], []]
-    # shun tem 4 vidas pq ele é o mais fraco, o q significa que ele deve viver
     cavaleiros_faltando = { 'Seya': VIDA, 'Ikki': VIDA, 'Shiryu': VIDA, 'Hyoga': VIDA, 'Shun': VIDA }
     total = 0
     minimo = 1000
+    minimo_vizinho = None
     maximo = 0
-    execucoes = 30
-    SimulatedAnnealing(dificuldade, cavaleiros, cavaleiros_faltando).simulated_annealing()
-    return
+    execucoes = 100
     for i in range(execucoes):
         resposta = SimulatedAnnealing(dificuldade, cavaleiros, cavaleiros_faltando).simulated_annealing()
         custo = resposta.get_cost()
+        if min(minimo, custo) == custo:
+            minimo = custo
+            minimo_vizinho = resposta
         minimo = min(minimo, custo)
         maximo = max(maximo, custo)
         total += custo
-        #print(resposta.get_cost())
-        #print(resposta.cavaleiros)
-        #print(resposta.cavaleiros_faltando)
-    print(total/execucoes)
-    print(minimo)
-    print(maximo)
-    print(resposta.cavaleiros)
+    print("Média:", total/execucoes)
+    print("Custo Mínimo:", minimo)
+    print("Solucao Mínima:", minimo_vizinho.cavaleiros)
+    print("Máximo:", maximo)
     return
     
 if __name__ == '__main__':
